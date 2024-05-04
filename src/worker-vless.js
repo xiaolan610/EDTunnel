5582ce60bc0825ffdee8c3ec1a608b52cfb6749c
lan <lan_at@163.com>
2024-05-04 18:59:28 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ac3a3a9e-8fc5-4345-8fda-e14c06e8a851';
 
-let proxyIP = '';
+let proxyIP = 'cdn.xn--b6gac.eu.org';
 
 
 if (!isValidUUID(userID)) {
